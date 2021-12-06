aa6bc27c19c15a3e37be79099ef39ac9a5791115
Claudia-teng <sydeng102@gmail.com>
2021-12-06 17:21:13 +0800

    index ui

--- src/app/components/bicycle-lane/bicycle-lane.component.ts
+++ src/app/components/bicycle-lane/bicycle-lane.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./bicycle-lane.component.sass']
 })
 export class BicycleLaneComponent {
-  
+  public options: any;
+  public overlays: any[];
+
+  public isRent: boolean = true;
+
+  ngOnInit() {
+    this.options = {
+        center: {lat: 36.890257, lng: 30.707417},
+        zoom: 12
+    };
+  }
+
+  public onToggleView(): void {
+    this.isRent = !this.isRent;
+  }
 }
