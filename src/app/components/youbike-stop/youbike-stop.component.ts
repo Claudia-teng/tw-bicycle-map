43c0abb88f0ddda5d284f1ffd54c0e2c40972011
Claudia-teng <sydeng102@gmail.com>
2021-12-06 20:18:49 +0800

    add current position

--- src/app/components/youbike-stop/youbike-stop.component.ts
+++ src/app/components/youbike-stop/youbike-stop.component.ts
@@ -1,24 +1,57 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+declare var google: any
 
 @Component({
   selector: 'youbike-stop',
   templateUrl: './youbike-stop.component.html',
   styleUrls: ['./youbike-stop.component.sass']
 })
+
 export class YoubikeStopComponent {
+  public loading: boolean = true;
+
   public options: any;
   public overlays: any[];
 
+  public currentLat: number;
+  public currentLng: number;
+
   public isRent: boolean = true;
 
+  constructor(private router: Router) {}
+
   ngOnInit() {
-    this.options = {
-        center: {lat: 36.890257, lng: 30.707417},
-        zoom: 12
-    };
+    this.loading = true;
+    this.findNearbyStation();
+  }
+
+  public navigateToIndex(): void {
+    this.router.navigate(['']);
   }
 
   public onToggleView(): void {
     this.isRent = !this.isRent;
+  }
+
+  public findNearbyStation(): void {
+    navigator.geolocation.watchPosition((position) => {
+      this.currentLat = position.coords.latitude;
+      this.currentLng = position.coords.longitude;
+      this.options = {
+        center: {lat: this.currentLat, lng: this.currentLng},
+        zoom: 12
+      };
+
+      this.overlays = [ 
+        new google.maps.Marker(
+          { position: 
+            { lat: this.currentLat,
+              lng: this.currentLng
+            },
+          })
+      ]
+      this.loading = false;
+    })
   }
 }
